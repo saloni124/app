15c3e6e872f74473219c8a9f85b63acba817a476
saloni124 <60982599+saloni124@users.noreply.github.com>
2025-11-24 18:25:45 -0800

    Add files via upload

---
+++ src/api/entities.js
@@ -0,0 +1,17 @@
+import { base44 } from './base44Client';
+
+
+export const Event = base44.entities.Event;
+
+export const Ticket = base44.entities.Ticket;
+
+export const EventAttendance = base44.entities.EventAttendance;
+
+export const EventReview = base44.entities.EventReview;
+
+export const Album = base44.entities.Album;
+
+
+
+// auth sdk:
+export const User = base44.auth;
